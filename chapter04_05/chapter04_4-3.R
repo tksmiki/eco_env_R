f26eed4d82ec20a22fded3d5e17dd3e14dd35dc1
tksmiki <83255215+tksmiki@users.noreply.github.com>
2023-09-10 07:59:28 +0900

    Add files via upload

---
+++ chapter04_05/chapter04_4-3.R
@@ -0,0 +1,20 @@
+####For chapter 04 section 4.3
+####For 4.3.1 (if/else/else if)
+
+#if statement
+a <- 3
+if(a > 3) {
+  cat("a is greater than 3.", "\n") #this iis executed when the condition is true
+} else {
+  cat("a is less than or equal to 3.", "\n") #This is executed when the condition is not true
+}
+#Check if TRUE or FALSE
+a < 3
+
+#Relational operators
+a < 3
+a >= 3
+a <= 3
+a == 3
+a != 2
+a == b
